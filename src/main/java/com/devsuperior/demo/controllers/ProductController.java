69dc7880923b9ed6d3fe6fbd4c941d9ebd80a645
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-09-09 00:10:31 -0300

    update Class ProductRepository com paginacao na JPQL usando countQuery, e COUNT e JOIN

--- src/main/java/com/devsuperior/demo/controllers/ProductController.java
+++ src/main/java/com/devsuperior/demo/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,7 @@ public class ProductController {
 	private ProductRepository productRepository;
 	
 	@GetMapping
-	public List<Product> findAll() {
-		return productRepository.searchAll();
+	public Page<Product> findAll(Pageable pageable) {
+		return productRepository.searchAll(pageable);
 	}
 }
--- src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.demo.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +13,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories")
 	List<Product> searchAll();
+
+	//Consulta JPQL não aceita paginação, para que consigamos, tem que fazer o seguinte macete:
+	//usar 'countQuery' para a JPA entender que é uma paginação, e dentro dele, usar 'COUNT' para ficar claro que se trata de contar as páginas ...
+	//e usar JOIN ao invés de join fetch.
+	@Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories",
+			countQuery = "SELECT COUNT(obj) FROM Product obj JOIN obj.categories")
+	Page<Product> searchAll(Pageable pageable);
+
 }
